6f9173d401227a796ae83b124ca4156124da60de
apjfan <apj.fan.imtiaz@gmail.com>
2016-11-04 23:39:32 +0600

    Update LDR_sensor.py

--- LDR_sensor.py
+++ LDR_sensor.py
@@ -16,22 +16,22 @@ GPIO.setup(led,GPIO.OUT)
 #__Function to read data from LDR sensor
 
 def r_time(data):
-count = 0
-GPIO.setup(data,GPIO.OUT)
-GPIO.output(data,GPIO.LOW)
-time.sleep(0.1)
-GPIO.setup(data,GPIO.IN)
-while( GPIO.input(data)==GPIO.LOW ):
-count+=1
-if(count>160000):
-break
-return count
+  count = 0
+  GPIO.setup(data,GPIO.OUT)
+  GPIO.output(data,GPIO.LOW)
+  time.sleep(0.1)
+  GPIO.setup(data,GPIO.IN)
+  while( GPIO.input(data)==GPIO.LOW ):
+    count+=1
+    if(count>160000):
+      break
+  return count
 #__Main body of the program__
 while True:
-print(r_time(data))
-if(r_time(data)>lightRange):
-GPIO.output(led,GPIO.HIGH)
-else:
-GPIO.output(led,GPIO.LOW)
+  print(r_time(data))
+  if(r_time(data)>lightRange):
+    GPIO.output(led,GPIO.HIGH)
+  else:
+    GPIO.output(led,GPIO.LOW)
 GPIO.cleanup();
 #__End of the program__
