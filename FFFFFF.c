70586cf5015206042ea529a24e1f7d834a1df7a3
apjfan <apj.fan.imtiaz@gmail.com>
2017-01-29 18:58:56 +0600

    Update FFFFFF.c

--- FFFFFF.c
+++ FFFFFF.c
@@ -13,7 +13,7 @@ String st="\0";
 String str="\0";
 String strArray[10];
 String fin="\0";
-int i,j,test=1;
+int i,j,test=1,cnt;
 int ldrData = 0;
 void setup(){
   RFID.begin(9600);
@@ -24,17 +24,20 @@ void setup(){
   ULTRAsetup();
 }
 void loop(){
-  while(1){
+   cnt=0;
+    //ser.write(0);
+    //delay(1000);
   getRfidData();
   getLdrData();
   getUltraData();
-  }
+  //delay(1000);
 }
 void RFIDsetup(){
   strArray[0]="24848484849704856515250513";
   strArray[1]="24948484851535568695766493";
   strArray[2]="25155484852657054515666513";
   pinMode(RFIDoutput,OUTPUT);
+  digitalWrite(RFIDoutput,0);
 }
 void LDRsetup(){
   pinMode(LDRoutput,OUTPUT);
@@ -45,55 +48,65 @@ void ULTRAsetup(){
     pinMode(echoPin,INPUT);  
 }
 void getRfidData(){
+ 
  fin=getData();
- Serial.println(fin);
- if(fin.length()){
+ //Serial.println(fin);
+ cnt=0;
+ if(fin.length()>=25){
            int chk=0;
            for(i=0;i<3;i++){              
                 if(fin==strArray[i]){
-                   Serial.println("Access Allowed!");
-                                ser.write(90);
-                                delay(100);
-                                ser.write(-90);
-                 
-                  fin="\0";
+                   //Serial.println("Access Allowed!");
+                 fin="\0";
                   str="\0";
                   st="\0";
                   test=0;
-                  chk=1; 
-            //      return;
-            break;
+                  chk=1;
+                  break;
                 }
             }
             if(chk==0){
-                     for(j=0;j<5;j++){
                         digitalWrite(RFIDoutput,0);
-                        delay(20);
-                      }
-                  Serial.println("Access denied!");
-                  fin="\0";
-                  test=1;
-                  delay(100);
-                  digitalWrite(RFIDoutput,1);
+                        delay(100);
+                        //Serial.println("Access denied!");
+                        fin="";
+                        test=1;
+                 // delay(100);
+                 digitalWrite(RFIDoutput,1);
+                 // delay(2);
+                  //digitalWrite(RFIDoutput,0);
+                        cnt=0;
             }
             else{
                   digitalWrite(RFIDoutput,1); 
+                  cnt++;
                   fin="\0";
             }
   }  
   delay(10);
   fin="";
+  if(cnt>0){
+                  ser.write(0);
+                  delay(500);
+                  //ser.write(90);
+                  //delay(100);
+                  ser.write(180);
+                  delay(500);
+                  cnt=0;
+    }
 }
 String getData(){
-    str="\0";
-    st="\0";
+    str="";
+    st="";
     while(RFID.available()>0){
          st = RFID.read();
          //getLdrData(); //...............
          str+=st;
+         //Serial.println(str);
+         delay(10);
         if(str.length()>=26){
-        Serial.println(str);
-        // delay(100);
+       // Serial.println(str);
+        delay(200);
            break;
          }
       }
@@ -101,10 +114,12 @@ String getData(){
 }
 void getLdrData(){
   ldrData = analogRead(ldrDataPin);
-  Serial.println(ldrData);
+  //Serial.println(ldrData);
   if(ldrData<50){
     digitalWrite(LDRoutput,0);
-  //  getUltraData();
+     //ser.write(0);
+     ////delay(1000);
+   // getRfidData();
   }
   else
     digitalWrite(LDRoutput,1);
@@ -126,5 +141,8 @@ void getUltraData(){
     digitalWrite(ULTRAoutput,0);
     delay(100);
   }  
-  Serial.println(dis);
+  //Serial.println(dis);
+  //ser.write(0);
+  delay(10);
+  //getRfidData();
 }
